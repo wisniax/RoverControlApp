14f08286d450e004e24ecb93f500d3c80342a0bc
wisniax <wisnia20012@gmail.com>
2023-03-26 14:59:33 +0200

    b

--- ControlTest.cs
+++ ControlTest.cs
@@ -13,6 +13,40 @@ public partial class ControlTest : Control
 		camera = new OnvifCameraController("placeholder", "admin", "admin");
 	}
 
+	public override void _Input(InputEvent passeEvent)
+	{
+		if (passeEvent is not InputEventKey eventKey) return;
+		if (!eventKey.Pressed) return;
+
+		switch (eventKey.Keycode)
+		{
+			case (int)Key.Up:
+				camera.MoveUp();
+				break;
+			case (int)KeyList.Down:
+				camera.MoveDown();
+				break;
+			case (int)KeyList.Left:
+				camera.MoveLeft();
+				break;
+			case (int)KeyList.Right:
+				camera.MoveRight();
+				break;
+			case (int)KeyList.PageUp:
+				camera.MoveUp();
+				break;
+			case (int)KeyList.PageDown:
+				camera.MoveDown();
+				break;
+			case (int)KeyList.Home:
+				camera.MoveHome();
+				break;
+			case (int)KeyList.End:
+				camera.MoveStop();
+				break;
+		}
+	}
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
