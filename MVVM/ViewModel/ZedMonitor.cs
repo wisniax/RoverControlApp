230fe91fbd900189f6311abb3ec65dbf7a12477e
Wojtuz <83223696+Wojtuz@users.noreply.github.com>
2024-03-12 00:16:35 +0100

    Added Quaterion to Euler conversion + MQTT comunication

--- MVVM/ViewModel/ZedMonitor.cs
+++ MVVM/ViewModel/ZedMonitor.cs
@@ -7,19 +7,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
+using RoverControlApp.MVVM.ViewModel;
 
 namespace RoverControlApp.MVVM.ViewModel;
 
 public partial class ZedMonitor : Panel
 {
-	//Fake data for UI testing
-	[Export]
-	float pitchDeg;
-	[Export]
-	float rollDeg;
+	//[Export]
+	double pitchDeg;
+	//[Export]
+	double rollDeg;
 
-	//Sprites for gyro visualisation
-	[Export]
+    //Sprites for gyro visualisation
+    [Export]
 	Sprite2D pitchVisualisation;
     [Export]
     Sprite2D rollVisualisation;
@@ -30,29 +31,95 @@ public partial class ZedMonitor : Panel
     [Export]
 	Label rollDisplay;
 
-    public override void _Ready()
-	{
-		
-	}
+	//Fake quaterion values for testing
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	[Export]
+	double X;
+	[Export]
+	double Y;
+	[Export]
+	double Z;
+	[Export]
+	double W;
+
+	double QuatX, QuatY, QuatZ, QuatW;
+
+	MqttClasses.GyroscopeSensorOutput ?Gyroscope;
+	string? msg;
+	int connected = 0;
+
 	public override void _Process(double delta)
 	{
-		VisualisationUpdate();
-		DisplayUpdate();
+		//Main cycle of the program with the MQTT connection check and quaternion processing
+		if (connected == 0)
+		{
+			ConnectionCheck();
+		}
+		else
+		{
+			PullGyroscope();
+			QuatW = Gyroscope.QuatW;
+			QuatX = Gyroscope.QuatX;
+			QuatY = Gyroscope.QuatY;
+			QuatZ = Gyroscope.QuatZ;
+			Roll();
+			Pitch();
+			VisualisationUpdate();
+			DisplayUpdate();
+		}
+
+
+
+		//Manual quaternion input for testing
+		//QuatW = W;
+		//QuatX = X;
+		//QuatY = Y;
+		//QuatZ = Z;
+		//Roll();
+		//Pitch();
+		//VisualisationUpdate();
+		//DisplayUpdate();
 	}
 
 	public void VisualisationUpdate()
 	{
-		pitchVisualisation.RotationDegrees = -pitchDeg;
-		rollVisualisation.RotationDegrees = rollDeg;
+		pitchVisualisation.RotationDegrees = -(float)pitchDeg;
+		rollVisualisation.RotationDegrees = (float)rollDeg;
 	}
 
 	public void DisplayUpdate()
 	{
-		pitchDisplay.Text = $"{pitchDeg} deg";
-        rollDisplay.Text = $"{rollDeg} deg";
+		pitchDisplay.Text = $"{Math.Round(pitchDeg, 2)} deg";
+        rollDisplay.Text = $"{Math.Round(rollDeg, 2)} deg";
     }
 
+	public void PullGyroscope()
+	{		
+		Gyroscope = JsonSerializer.Deserialize<MqttClasses.GyroscopeSensorOutput>(msg);
+		double[] Quat = {Gyroscope.QuatW, Gyroscope.QuatX, Gyroscope.QuatY, Gyroscope.QuatZ };
+	}
+	public void ConnectionCheck()
+	{
+        
+        msg = MainViewModel.MqttClient?.GetReceivedMessageOnTopicAsString(MainViewModel.Settings?.Settings?.Mqtt.TopicGyroscopeSensorOutput);
+        if (msg != null)
+		{
+			connected = 1;
+        }
+    }
+
+	public double ConvertToDegrees(double radians)
+	{
+		return radians * (180.0 / (float)Math.PI);
+	}
+
+	public void Roll()
+	{
+        rollDeg = ConvertToDegrees(Math.Atan2(2 * (QuatW * QuatX + QuatY * QuatZ), 1 - 2 * (QuatX * QuatX + QuatY * QuatY)));
+    }
+	public void Pitch()
+	{
+        pitchDeg = ConvertToDegrees(Math.Asin(2 * (QuatW * QuatY - QuatZ * QuatX)));
+	}
 
 }
