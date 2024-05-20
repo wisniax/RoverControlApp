5caee8b38227cd56969b22248b2cb54398fe40ed
Marcin Walczyk <marcinwal9@gmail.com>
2024-05-20 19:47:18 +0200

    add GetAllTopicsToSubscribe method as extension to Mqtt settings

---
+++ MVVM/Model/Settings/MqttSubscribedExtension.cs
@@ -0,0 +1,25 @@
+﻿using MQTTnet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverControlApp.MVVM.Model.Settings 
+{
+	public static class MqttSubscribedExtension
+	{
+		public static (string, MqttQualityOfServiceLevel)[] GetAllTopicsToSubscribe(this Mqtt mqttSettings)
+			=>
+			[
+				( mqttSettings.TopicRoverStatus, MqttQualityOfServiceLevel.ExactlyOnce ),
+				( mqttSettings.TopicMissionStatus, MqttQualityOfServiceLevel.ExactlyOnce ),
+				( mqttSettings.TopicKmlListOfActiveObj, MqttQualityOfServiceLevel.ExactlyOnce ),
+				( mqttSettings.TopicRoverFeedback, MqttQualityOfServiceLevel.ExactlyOnce ),
+
+				( mqttSettings.TopicWheelFeedback, MqttQualityOfServiceLevel.AtMostOnce ),
+				( mqttSettings.TopicEStopStatus, MqttQualityOfServiceLevel.AtMostOnce ),
+			];
+		
+	}
+}
