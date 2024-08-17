6c926b226baac50e942a5f5021b14caf716e2ef1
Marcin Walczyk <marcinwal9@gmail.com>
2024-08-17 22:29:58 +0200

    add new IRoverManipulatorController

---
+++ Core/RoverControllerPresets/IRoverManipulatorController.cs
@@ -0,0 +1,19 @@
+﻿using static RoverControlApp.Core.MqttClasses;
+
+namespace RoverControlApp.Core.RoverControllerPresets;
+
+public interface IRoverManipulatorController
+{
+	/// <summary>
+	/// Probes Godot.Input and returns ManipulatorControl
+	/// </summary>
+	public ManipulatorControl CalculateMoveVector();
+
+	/// <summary>
+	/// Compares two ManipulatorControl states and determines if change is big enough, to be considered
+	/// </summary>
+	/// <returns>true if changed</returns>
+	public bool IsMoveVectorChanged(ManipulatorControl currentState, ManipulatorControl lastState) =>
+		!currentState.Equals(lastState);
+
+}
