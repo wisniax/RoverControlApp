cc3705321642b441700ef32fdf7368695495bc32
Marcin Walczyk <marcinwal9@gmail.com>
2024-08-17 23:09:16 +0200

    add SingleAxisManipulatorController to new namespace

---
+++ Core/RoverControllerPresets/ManipulatorControllers/SingleAxisManipulatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Godot;
+using static RoverControlApp.Core.MqttClasses;
+
+namespace RoverControlApp.Core.RoverControllerPresets.ManipulatorControllers;
+
+public class SingleAxisManipulatorController : IRoverManipulatorController
+{
+	public ManipulatorControl CalculateMoveVector()
+	{
+		float velocity = Input.GetAxis("manipulator_speed_backward", "manipulator_speed_forward");
+		if (Mathf.Abs(velocity) < LocalSettings.Singleton.Joystick.Deadzone) 
+			velocity = 0f;
+
+		ManipulatorControl manipulatorControl = new()
+		{
+			Axis1 = Input.IsActionPressed("manipulator_axis_1") ? velocity : 0f,
+			Axis2 = Input.IsActionPressed("manipulator_axis_2") ? velocity : 0f,
+			Axis3 = Input.IsActionPressed("manipulator_axis_3") ? velocity : 0f,
+			Axis4 = Input.IsActionPressed("manipulator_axis_4") ? velocity : 0f,
+			Axis5 = Input.IsActionPressed("manipulator_axis_5") ? velocity : 0f,
+
+			Gripper = Input.IsActionPressed("manipulator_gripper") ? velocity : 0f
+		};
+
+		return manipulatorControl;
+	}
+}
