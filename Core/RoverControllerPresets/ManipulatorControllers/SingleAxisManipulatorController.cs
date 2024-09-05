effecaf3deba25792ef02cc1e3b2dfee25708d98
Wojciech Murlikiewicz <250984@edu.p.lodz.pl>
2024-09-05 18:30:43 +0200

    6 Axis (facebuttons + bumpers) + gripper (RB+LB)

--- Core/RoverControllerPresets/ManipulatorControllers/SingleAxisManipulatorController.cs
+++ Core/RoverControllerPresets/ManipulatorControllers/SingleAxisManipulatorController.cs
@@ -12,16 +12,33 @@ public class SingleAxisManipulatorController : IRoverManipulatorController
 		if (Mathf.Abs(velocity) < LocalSettings.Singleton.Joystick.Deadzone) 
 			velocity = 0f;
 
-		ManipulatorControl manipulatorControl = new()
+		ManipulatorControl manipulatorControl = null;
+		if (Input.IsActionPressed("manipulator_axis_5") && Input.IsActionPressed("manipulator_axis_6"))
 		{
-			Axis1 = Input.IsActionPressed("manipulator_axis_1") ? velocity : 0f,
-			Axis2 = Input.IsActionPressed("manipulator_axis_2") ? velocity : 0f,
-			Axis3 = Input.IsActionPressed("manipulator_axis_3") ? velocity : 0f,
-			Axis4 = Input.IsActionPressed("manipulator_axis_4") ? velocity : 0f,
-			Axis5 = Input.IsActionPressed("manipulator_axis_5") ? velocity : 0f,
+			manipulatorControl = new()
+			{
+				Axis1 = 0f,
+				Axis2 = 0f,
+				Axis3 = 0f,
+				Axis4 = 0f,
+				Axis5 = 0f,
+				Axis6 = 0f,
 
-			Gripper = Input.IsActionPressed("manipulator_gripper") ? velocity : 0f
-		};
+				Gripper = velocity
+			};
+		}
+		else
+			manipulatorControl = new()
+			{
+				Axis1 = Input.IsActionPressed("manipulator_axis_1") ? velocity : 0f,
+				Axis2 = Input.IsActionPressed("manipulator_axis_2") ? velocity : 0f,
+				Axis3 = Input.IsActionPressed("manipulator_axis_3") ? velocity : 0f,
+				Axis4 = Input.IsActionPressed("manipulator_axis_4") ? velocity : 0f,
+				Axis5 = Input.IsActionPressed("manipulator_axis_5") ? velocity : 0f,
+				Axis6 = Input.IsActionPressed("manipulator_axis_6") ? velocity : 0f,
+
+				Gripper = Input.IsActionPressed("manipulator_gripper") ? velocity : 0f
+			};
 
 		return manipulatorControl;
 	}
