45464b61d338914e890a0e5e216e82776e2a2637
Marcin Walczyk <marcinwal9@gmail.com>
2025-05-26 19:45:13 +0200

    add InputHelpHint

---
+++ MVVM/ViewModel/InputHelpHint.cs
@@ -0,0 +1,265 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Godot;
+
+using RoverControlApp.Core;
+
+using HintInit = System.Collections.Generic.KeyValuePair<string, Godot.Collections.Array<Godot.InputEvent>>;
+
+[Tool]
+public partial class InputHelpHint : Panel
+{
+	#region Enums
+
+	public enum HintVisibility
+	{
+		None = 0,
+		Kb = 1,
+		Joy = 2,
+	}
+
+	#endregion Enums
+
+
+	#region Fields
+
+	[ExportGroup(".internal", "_")]
+
+	[Export]
+	private HBoxContainer _kbHelp = null!;
+
+	[Export]
+	private HBoxContainer _joyHelp = null!;
+
+	[Export]
+	private Label _kbHelpEventLabel = null!;
+
+	[Export]
+	private Label _joyHelpEventLabel = null!;
+
+	[Export]
+	private Label _kbHelpActionLabel = null!;
+
+	[Export]
+	private Label _joyHelpActionLabel = null!;
+
+	private HintInit _hintInformation = new("<ACTION_INVALID>", []);
+	private HintVisibility _visibleHint = HintVisibility.Kb;
+	private bool _showEmpty = false;
+
+	private List<string> _kbHints = [];
+	private List<string> _joyHints = [];
+
+	#endregion Fields
+
+	#region Properties
+
+	public HintInit HintInformation
+	{
+		get => _hintInformation;
+		set
+		{
+			_hintInformation = value;
+			if (IsInsideTree())
+				CallDeferred(MethodName.SetupHint);
+		}
+	}
+
+	[Export]
+	public HintVisibility VisibleHint
+	{
+		get => _visibleHint;
+		set
+		{
+			_visibleHint = value;
+			if (IsInsideTree())
+				CallDeferred(MethodName.ChangeVisible);
+		}
+	}
+
+	[Export]
+	public bool ShowEmpty
+	{
+		get => _showEmpty;
+		set
+		{
+			_showEmpty = value;
+			if (IsInsideTree())
+				CallDeferred(MethodName.ChangeVisible);
+		}
+	}
+
+	[Export]
+	public Color ActionActiveColor = Colors.LimeGreen;
+
+	[Export]
+	public Color ActionStaticColor = Colors.White;
+
+	#endregion Properties
+
+	#region Godot
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		SetupHint();
+		ChangeVisible();
+	}
+
+	public override void _Process(double delta)
+	{
+		if (!Engine.IsEditorHint())
+		{
+			float actionStr = Input.GetActionStrength(_hintInformation.Key, exactMatch: true);
+
+			Color newColor = ActionStaticColor.Lerp(
+				ActionActiveColor,
+				actionStr
+			);
+
+			_kbHelpActionLabel.AddThemeColorOverride(
+				"font_color",
+				newColor
+			);
+
+			_joyHelpActionLabel.AddThemeColorOverride(
+				"font_color",
+				newColor
+			);
+		}
+	}
+
+	public override Vector2 _GetMinimumSize()
+	{
+		return VisibleHint switch
+		{
+			HintVisibility.Kb when _kbHelp.Visible => _kbHelp.GetCombinedMinimumSize(),
+			HintVisibility.Joy when _joyHelp.Visible => _joyHelp.GetCombinedMinimumSize(),
+			_ => Vector2.Zero,
+		};
+	}
+
+	#endregion Godot
+
+	#region Methods
+
+	private string GetJoyAxisString(in InputEventJoypadMotion inputJoyAxis)
+	{
+		StringBuilder eventStrBuilder = new();
+		eventStrBuilder.Append("JAx_");
+
+		switch (inputJoyAxis.Axis)
+		{
+			case JoyAxis.LeftX:
+			case JoyAxis.LeftY:
+				eventStrBuilder.Append($"JoyLeft");
+				break;
+			case JoyAxis.RightX:
+			case JoyAxis.RightY:
+				eventStrBuilder.Append($"JoyRight");
+				break;
+			case JoyAxis.TriggerLeft:
+			case JoyAxis.TriggerRight:
+				eventStrBuilder.Append(inputJoyAxis.Axis);
+				break;
+		}
+
+		switch (inputJoyAxis.Axis)
+		{
+			case JoyAxis.RightX when inputJoyAxis.AxisValue < 0:
+			case JoyAxis.LeftX when inputJoyAxis.AxisValue < 0:
+				eventStrBuilder.Append($"_ToLeft");
+				break;
+			case JoyAxis.RightX when inputJoyAxis.AxisValue > 0:
+			case JoyAxis.LeftX when inputJoyAxis.AxisValue > 0:
+				eventStrBuilder.Append($"_ToRight");
+				break;
+			case JoyAxis.RightY when inputJoyAxis.AxisValue < 0:
+			case JoyAxis.LeftY when inputJoyAxis.AxisValue < 0:
+			case JoyAxis.TriggerLeft when inputJoyAxis.AxisValue > 0:
+			case JoyAxis.TriggerRight when inputJoyAxis.AxisValue > 0:
+				eventStrBuilder.Append($"_ToDown");
+				break;
+			case JoyAxis.RightY when inputJoyAxis.AxisValue > 0:
+			case JoyAxis.LeftY when inputJoyAxis.AxisValue > 0:
+			case JoyAxis.TriggerLeft when inputJoyAxis.AxisValue < 0:
+			case JoyAxis.TriggerRight when inputJoyAxis.AxisValue < 0:
+				eventStrBuilder.Append($"_ToUp");
+				break;
+		}
+		return eventStrBuilder.ToString();
+	}
+
+	private void SetupHint()
+	{
+		_kbHints.Clear();
+		_joyHints.Clear();
+		foreach (var input in _hintInformation.Value.Where((i) => i is InputEventKey))
+		{
+			if (input is InputEventKey inputKbKey)
+			{
+				if (inputKbKey.Keycode != Key.None)
+					_kbHints.Add($"KBt_{OS.GetKeycodeString(inputKbKey.Keycode)}");
+				if (inputKbKey.KeyLabel != Key.None)
+					_kbHints.Add($"KBL_{OS.GetKeycodeString(inputKbKey.KeyLabel)}");
+				if (inputKbKey.PhysicalKeycode != Key.None)
+					_kbHints.Add($"KBP_{OS.GetKeycodeString(inputKbKey.PhysicalKeycode)}");
+			}
+		}
+
+		foreach (var input in _hintInformation.Value.Where((i) => i is InputEventJoypadButton or InputEventJoypadMotion))
+		{
+			if (input is InputEventJoypadButton inputJoyKey && inputJoyKey.ButtonIndex != JoyButton.Invalid)
+				_joyHints.Add($"JBt_{inputJoyKey.ButtonIndex}");
+			if (input is InputEventJoypadMotion inputJoyAxis && inputJoyAxis.Axis != JoyAxis.Invalid)
+				_joyHints.Add(GetJoyAxisString(inputJoyAxis));
+		}
+
+		_kbHelpActionLabel.Text = _hintInformation.Key;
+		_joyHelpActionLabel.Text = _hintInformation.Key;
+
+		EventLogger.LogMessageDebug(nameof(InputHelpMaster) + $"/{_hintInformation.Key}", EventLogger.LogLevel.Verbose, $"K:{_kbHints.Count} J:{_joyHints.Count}");
+
+		CycleHints(0);
+	}
+
+	private void ChangeVisible()
+	{
+		switch (VisibleHint)
+		{
+			case HintVisibility.Kb when _kbHints.Count > 0 || _showEmpty:
+				_kbHelp.Visible = true;
+				_joyHelp.Visible = false;
+				break;
+			case HintVisibility.Joy when _joyHints.Count > 0 || _showEmpty:
+				_kbHelp.Visible = false;
+				_joyHelp.Visible = true;
+				break;
+			default:
+				_kbHelp.Visible = false;
+				_joyHelp.Visible = false;
+				break;
+		}
+
+		UpdateMinimumSize();
+	}
+
+	private void CycleHints(int cycle)
+	{
+		if (_kbHints.Count > 0)
+			_kbHelpEventLabel.Text = _kbHints[cycle % _kbHints.Count];
+		else
+			_kbHelpEventLabel.Text = "<KBt_INVALID>";
+
+		if (_joyHints.Count > 0)
+			_joyHelpEventLabel.Text = _joyHints[cycle % _joyHints.Count];
+		else
+			_joyHelpEventLabel.Text = "<JBt_INVALID>";
+
+		UpdateMinimumSize();
+	}
+
+	#endregion Methods
+}
