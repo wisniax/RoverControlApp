f15e746f13f3d06649e5597c0f77e8f2fd06c698
Marcin Walczyk <marcinwal9@gmail.com>
2025-08-28 14:48:56 +0200

    add ModeQuickSelect

--- Core/RoverControllerPresets/ControlModeControllers/StandardModeController.cs.cs
+++ Core/RoverControllerPresets/ControlModeControllers/StandardModeController.cs.cs
@@ -3,59 +3,87 @@ using System;
 using Godot;
 using Godot.Collections;
 
+using RoverControlApp.MVVM.Model;
+
 using static RoverControlApp.Core.MqttClasses;
 
 namespace RoverControlApp.Core.RoverControllerPresets.ControlModeControllers;
 
 public class StandardModeController : IControlModeController
 {
-    private const ulong _fastSelectTime = 300;
-    private int _fastSelectCount = 0;
-    private ulong _fastSelectLastPress = 0;
+	private static readonly string[] _usedActions =
+	[
+		"controlmode_estop",
+		"controlmode_change",
+		"controlmode_drive",
+		"controlmode_manipulator",
+		"controlmode_sampler",
+		"controlmode_autonomy",
+	];
 
-    private static readonly string[] _usedActions =
-    [
-        "controlmode_estop",
-        "controlmode_change",
-    ];
+	TimeSpan? estopStart;
 
-    public bool EstopReq()
-    {
-        return Input.IsActionJustPressed("controlmode_estop", exactMatch: true);
-    }
+	public bool EstopReq()
+	{
+		if (Input.IsActionJustPressed("controlmode_estop", exactMatch: true))
+		{
+			estopStart = System.DateTime.Now.TimeOfDay;
+			return false;
+		}
 
-    public ControlMode GetControlMode(in InputEvent inputEvent, in ControlMode lastState)
-    {
-        ControlMode newState = lastState;
+		if (Input.IsActionJustReleased("controlmode_estop", exactMatch: true) && estopStart is not null)
+		{
+			if ((System.DateTime.Now.TimeOfDay - estopStart).Value.TotalSeconds < 5)
+				return true;
+		}
 
-        if (inputEvent.IsActionPressed("controlmode_change", exactMatch: true))
-        {
-            if (Time.GetTicksMsec() - _fastSelectLastPress < _fastSelectTime)
-            {
-                _fastSelectCount++;
-            }
-            else
-            {
-                _fastSelectCount = 0;
-            }
+		return false;
+	}
 
-            _fastSelectLastPress = Time.GetTicksMsec();
 
-            if (_fastSelectCount > 0)
-            {
-                newState = (ControlMode)(_fastSelectCount % Enum.GetNames<ControlMode>().Length);
-            }
-            else
-            {
-                if ((int)lastState + 1 >= Enum.GetNames<ControlMode>().Length)
-                    newState = ControlMode.Rover;
-                else
-                    newState++;
-            }
-        }
-        return newState;
-    }
+	public ControlMode GetControlMode(in InputEvent inputEvent, in ControlMode lastState)
+	{
+		ControlMode newState = lastState;
 
-    public System.Collections.Generic.Dictionary<string, Array<InputEvent>> GetInputActions() =>
-        IActionAwareController.FetchAllActionEvents(_usedActions);
+		if (Input.IsActionPressed("controlmode_estop") || PressedKeys.IsInputFromKeyboard(inputEvent))
+		{
+			if (inputEvent.IsActionPressed("controlmode_drive", exactMatch: true))
+			{
+				estopStart = null;
+				newState = ControlMode.Rover;
+			}
+			else if (inputEvent.IsActionPressed("controlmode_manipulator", exactMatch: true))
+			{
+				estopStart = null;
+				newState = ControlMode.Manipulator;
+			}
+			else if (inputEvent.IsActionPressed("controlmode_sampler", exactMatch: true))
+			{
+				estopStart = null;
+				newState = ControlMode.Sampler;
+			}
+			else if (inputEvent.IsActionPressed("controlmode_autonomy", exactMatch: true))
+			{
+				estopStart = null;
+				newState = ControlMode.Autonomy;
+			}
+		}
+		else if (inputEvent.IsActionPressed("controlmode_change", exactMatch: true))
+		{
+			if ((int)lastState + 1 >= Enum.GetNames<ControlMode>().Length)
+				newState = ControlMode.Rover;
+			else
+				newState++;
+		}
+		return newState;
+	}
+
+	public System.Collections.Generic.Dictionary<string, Array<InputEvent>> GetInputActions() =>
+		IActionAwareController.FetchAllActionEvents(_usedActions);
+
+	public string GetInputActionsAdditionalNote() =>
+	"""
+	To quick select control mode on the controller:
+	 HOLD 'controlmode_estop' and PRESS desired mode.
+	""";
 }
