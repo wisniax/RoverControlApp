6e5e4f7344a4ac26864253ffea8a81dc8601cccd
Wojciech Murlikiewicz <wojtuzmurlik@gmail.com>
2026-08-30 19:45:25 +0200

    Civilised way of doing it (Someone some day is gonna wonder why using entertree breaks things xd)

--- MVVM/ViewModel/StatusPanels/EStopPanel.cs
+++ MVVM/ViewModel/StatusPanels/EStopPanel.cs
@@ -1,31 +1,8 @@
-using Godot;
-using System;
-
-public partial class EStopPanel : Control
+public partial class EStopPanel : StatusPanel
 {
-	[Export] Panel _hiddenStatePanel = null!;
-	[Export] Panel _shownStatePanel = null!;
-
-	public void PanelPressed()
-	{
-		if (_hiddenStatePanel.Visible)
-		{
-			_hiddenStatePanel.Visible = false;
-			_shownStatePanel.Visible = true;
-			this.CustomMinimumSize = _shownStatePanel.Size;
-		}
-		else
-		{
-			_hiddenStatePanel.Visible = true;
-			_shownStatePanel.Visible = false;
-			this.CustomMinimumSize = _hiddenStatePanel.Size;
-		}
-	}
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		PanelPressed();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
